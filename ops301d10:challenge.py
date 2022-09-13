f67f2f02f89b4b17499c9784672a6ff2c9e813ac
AuspiciousApple <112282509+AuspiciousApple@users.noreply.github.com>
2022-09-12 22:55:03 -0400

    Create ops301d10:challenge.py

---
+++ ops301d10:challenge.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+a = 10
+b = 20
+c = 30
+d = 40
+e = 50
+
+
+if a == b:
+	print("a and b are the equal")
+if a != b:
+	print("a does not equal b")
+if a < b:
+	print("a is less than b")
+if a <= b:
+	print("a is less than or equal to b")
+if a > b:
+	print("a is greater than b")
+if a >= b:
+	print("a is greater than or equal to b")
+	
+if a == b:
+	print("A is equal to B")
+elif a != b:
+	print("A does not equal B")
+	
+if a < b:
+	print("A is less than B")
+elif a > b:
+	print("A is greater than B")
+else:
+	print ("A is equal to B")
+ 
